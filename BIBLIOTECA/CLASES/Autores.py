5ab0b3b33b0898314803c3d78ae7e5207cc218ee
Johan27488 <johanguttierrez258@gmail.com>
2024-09-29 14:07:32 -0300

    actualizacion3

--- BIBLIOTECA/CLASES/Autores.py
+++ BIBLIOTECA/CLASES/Autores.py
@@ -1,7 +1,12 @@
-# POO-Johan
+from datetime import datetime
+import re
+from rut_chile import rut_chile
 
+class Paises:
+    def __init__(self, codigo_pais):
+        self.codigo_pais = codigo_pais
 
-class Autor(paises):
+class Autor(Paises):
     def __init__(self, id_autor, nombre_autor, codigo_pais, fecha_nac, fecha_def, biografia_autor, foto_autor):
         super().__init__(codigo_pais)
         self.id_autor = id_autor
@@ -11,7 +16,7 @@ class Autor(paises):
         self.biografia_autor = biografia_autor
         self.foto_autor = foto_autor
     
-    def manejo_fechas(fecha):
+    def manejo_fechas(self, fecha):
+        """Convierte una fecha en formato 'dd/mm/yyyy' a 'yyyy-mm-dd'."""
         fecha_dt = datetime.strptime(fecha, '%d/%m/%Y')
-        fecha_str = fecha_dt.strftime('%Y-%m-%d')
-        return fecha_str
+        return fecha_dt.strftime('%Y-%m-%d')
